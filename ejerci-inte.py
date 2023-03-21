60fac4924e379dcfdc07016570816902882e3acf
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-03-20 23:23:21 -0300

    adding exercises 6-8

--- ejerci-inte.py
+++ ejerci-inte.py
@@ -57,12 +57,130 @@ dictToTup(strToDict("la casa esta en la colina")) """
     except ValueError:
         get_int()
     return int """
-def get_int():
+""" def get_int():
     number = ''
     while not type(number) is int:
         try:
-            number = input("insert a number: ")
+            number = int(input("insert a number: "))
         except ValueError:
-            print("SOlo numeros")
+            number = int(input("insert a number: "))
     return number
-get_int()
+get_int() """
+
+# 6. Crear una clase llamada Persona. Sus atributos son: nombre, edad y DNI. Construya los
+# siguientes métodos para la clase:
+#  Un constructor, donde los datos pueden estar vacíos.
+#  Los setters y getters para cada uno de los atributos. Hay que validar las entradas de
+# datos.
+#  mostrar(): Muestra los datos de la persona.
+#  Es_mayor_de_edad(): Devuelve un valor lógico indicando si es mayor de edad.
+
+class Persona():
+    def __init__(self, nombre, telefono, dni):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.dni = dni
+    
+    def mostrar(self):
+        print(self.nombre, self.telefono, self.dni)
+    
+    @property
+    def nombre(self):
+        return self.nombre
+    @property
+    def telefono(self):
+        return self.telefono
+    @property
+    def dni(self):
+        return self.dni
+    
+    @nombre.setter
+    def nombre(self, a):
+        if type(a) != str:
+           raise TypeError("ingrese una cadena de texto")
+        else:
+            self.nombre = a
+            print("nombre modificado")
+        
+    @telefono.setter
+    def telefono(self, a):
+        if len(a) < 6:
+            raise ValueError("ingrese telefono valido (8 caracteres)")
+        else:
+            self.telefono = a
+            print("telefono modificado")
+            
+    @dni.setter
+    def dni(self, a):
+        # while not type(a) == int or len(a) < 6:
+        #     print("Ingrese un dni valido")
+        self.dni = a
+        print("dni modificado")
+        
+    
+# 7. Crea una clase llamada Cuenta que tendrá los siguientes atributos: titular (que es una
+# persona) y cantidad (puede tener decimales). El titular será obligatorio y la cantidad es
+# opcional. Crear los siguientes métodos para la clase:
+#  Un constructor, donde los datos pueden estar vacíos.
+#  Los setters y getters para cada uno de los atributos. El atributo no se puede modificar
+# directamente, sólo ingresando o retirando dinero.
+#  mostrar(): Muestra los datos de la cuenta.
+#  ingresar(cantidad): se ingresa una cantidad a la cuenta, si la cantidad introducida es
+# negativa, no se hará nada.
+#  retirar(cantidad): se retira una cantidad a la cuenta. La cuenta puede estar en números
+# rojos.
+
+class Cuenta(Persona):
+    def __init__(self, nombre, telefono, dni, cantidad=0):
+        self.titular = super().__init__(nombre, telefono, dni)
+        self.__cantidad = cantidad
+        
+    @property
+    def titular(self):
+        return self.titular
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    def mostrar(self):
+        print(self.titular, self.__cantidad)
+        
+    def ingresar(self, a):
+        if self.__cantidad < 0:
+            print('Ingrese un numero positivo')
+        print(f'saldo actual {self.__cantidad}')
+        
+    def retirar(self, a):
+        self.__cantidad -= a
+        print(f'saldo actual {self.__cantidad}')
+    
+    
+    
+# 8. Vamos a definir ahora una “Cuenta Joven”, para ello vamos a crear una nueva clase
+# CuantaJoven que deriva de la clase creada en el punto 7. Cuando se crea esta nueva clase,
+# además del titular y la cantidad se debe guardar una bonificación que estará expresada en
+# tanto por ciento. Crear los siguientes métodos para la clase:
+#  Un constructor.
+#  Los setters y getters para el nuevo atributo.
+#  En esta ocasión los titulares de este tipo de cuenta tienen que ser mayor de edad, por lo
+# tanto hay que crear un método es_titular_valido() que devuelve verdadero si el titular es
+# mayor de edad pero menor de 25 años y falso en caso contrario.
+#  Además, la retirada de dinero sólo se podrá hacer si el titular es válido.
+#  El método mostrar() debe devolver el mensaje de “Cuenta Joven” y la bonificación de la
+# cuenta.
+
+
+class CuentaJover(Cuenta):
+    def __init__(self,titular, bonificacion,):
+        super().__init__(titular, cantidad=0)
+        
+    def es_titular_valido(self):
+        print(self.titular, self.cantidad)
+    
+    def mostrar(self):
+        print("Cuenta Joven", self.titular, self.cantidad)
+        
+    def retirar(self, a):
+        self.cantidad -= a
+        print(f'saldo actual {self.cantidad}')
+
